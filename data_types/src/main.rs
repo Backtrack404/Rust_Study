7ff518a397eed564d80ef8bbcf55985cf6320518
Backtrack404 <jun48470@gmail.com>
2022-05-03 15:15:16 +0000

    데이터 타입 코드 수정

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,11 +1,12 @@
-// fn main(){
-//     let guess: u32 = "42".parse().expect("Not a number!");
-
-//     println!("val: {}", guess);
-// }
-
-
 fn main(){
-    let guess = "42".parse().expect("Not a number!");
-    println!("val: {}",guess)
+    let guess: u32 = "42".parse().expect("Not a number!");
+
+    println!("val: {}", guess);
 }
+
+
+// 실행 안됨
+// fn main(){
+//     let guess = "42".parse().expect("Not a number!");
+//     println!("val: {}", guess)
+// }
