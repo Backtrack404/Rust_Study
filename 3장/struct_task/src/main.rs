59cbc05e7d601bba289e2e5073bad1c0db8619b1
Backtrack404 <jun48470@gmail.com>
2022-05-09 06:27:58 +0000

    구조체

---
+++ 3장/struct_task/src/main.rs
@@ -0,0 +1,60 @@
+struct User{
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+// fn main(){
+//     let mut user1 = User{
+//         email:String::from("someone@example.com"),
+//         username:String::from("someusername123"),
+//         active: true,
+//         sign_in_count: 1,
+//     };
+
+//     user1.email = String::from("anotheremail@example.com");
+
+//     println!("user email: {}", user1.email);
+// }
+
+// fn main(){
+//     let email = String::from("someone@example.com");
+//     let username = String::from("someusername123");
+//     let user = build_user(email, username);
+
+//     println!("username: {}", user.username);
+//     println!("email: {}", user.email);
+//     println!("active: {}", user.active);
+//     println!("sign_in_count: {}", user.sign_in_count);
+    
+// }
+
+// fn build_user(email: String, username: String) -> User {
+//     User{
+//         email: email,
+//         username: username,
+//         active: true,
+//         sign_in_count: 1,
+//     }
+// }
+
+fn main(){
+    let email = String::from("someone@example.com");
+    let username = String::from("someusername123");
+    let user = build_user(email, username);
+
+    println!("username: {}", user.username);
+    println!("email: {}", user.email);
+    println!("active: {}", user.active);
+    println!("sign_in_count: {}", user.sign_in_count);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
