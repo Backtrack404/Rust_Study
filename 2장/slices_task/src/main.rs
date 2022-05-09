5c1abc9e02a080a1d26f9d6e5d4d80ee487ad7c7
Backtrack404 <jun48470@gmail.com>
2022-05-09 05:32:41 +0000

    슬라이스

--- 2장/slices_task/src/main.rs
+++ 2장/slices_task/src/main.rs
@@ -1,18 +1,72 @@
+// fn main(){
+//     let s = String::from("hello world!");
+
+//     let len = first_word(&s);
+
+//     println!("len: {}",len);
+// }   
+
+// fn first_word(s: &String) -> usize{
+//     let bytes = s.as_bytes();
+
+//     for (i, &item) in bytes.iter().enumerate(){
+//         if item == b' '{
+//             return i;
+//         }
+//     }
+//     s.len()
+// }
+
+// fn main(){
+//     let s = String::from("hello world");
+//     let str = first_word(&s);
+    
+//     // s.clear();
+    
+//     println!("{}", str);
+// }
+
+// fn first_word(s: &String) -> &str{
+//     let bytes = s.as_bytes();
+//     let void = b' ';
+//     println!("void: {}", void);    
+
+//     for (i, &item) in bytes.iter().enumerate(){
+//         println!("i: {}\nitem:{}", i, &item);
+
+//         if item == b' '{
+//             return &s[0..i];
+//         }
+//     }
+//     &s[..]
+// }
+
 fn main(){
-    let s = String::from("hello world!");
+    let my_string = String::from("hello world");
 
-    let len = first_word(&s);
+    // first_word가 'String'의 슬라이스로 동작
+    let world = first_word(&my_string[..]);
+    println!("{}", world);
 
-    println!("len: {}",len);
-}   
+    let my_string_literal = "hello world";
 
-fn first_word(s: &String) -> usize{
+    // first_word가 스트링 리터럴의 슬라이스로 동작/
+    let world = first_word(&my_string_literal[..]);
+    println!("{}", world);
+
+    // 스트링 리터럴은 *또한* 스트링 슬라이스이기 떄문에, 
+    // 아래 코드도 슬라이스 문법 없이 동작.
+    let world = first_word(my_string_literal);
+    println!("{}", world);
+}
+
+fn first_word(s: &str) -> &str{
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate(){
-        if item == b' '{
-            return i;
+        if item == b' ' {
+            return &s[0..i];
         }
     }
-    s.len()
+    &s[..]
 }
