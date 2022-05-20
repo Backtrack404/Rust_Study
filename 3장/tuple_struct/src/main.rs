919cd6711dbafef95d8d3c6244c4aa7fc3571e19
Backtrack404 <jun48470@gmail.com>
2022-05-20 13:25:16 +0000

    Tuple test

--- 3장/tuple_struct/src/main.rs
+++ 3장/tuple_struct/src/main.rs
@@ -1,10 +1,10 @@
 struct Color(i32, i32, i32);
-struct Point(i32, i32, i32);
+struct Point(i32, i32, f32);
 
 fn main(){
-    let black = Color(0,0,0);
-    let origin = Point(0,0,0);
+    let black = Color(255,0,255);
+    let origin = Point(0,0,0.1);
 
     println!("{}", black.0);
-    println!("{}", origin.0);
+    println!("{}", origin.2);
 }
